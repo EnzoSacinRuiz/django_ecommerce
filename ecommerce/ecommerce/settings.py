5f923128b43d9a39db2e42b1fe690c87b3ae232b
EnzoSacinRuiz <enzo.sacin@utec.edu.pe>
2024-03-07 22:07:44 -0500

    rendering store

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -124,7 +124,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/images/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
-
+from .models import Product  # Make sure to import the Product model
 
 def store(request):
-     context = {}
-     return render(request, 'store/store.html', context)
+    products = Product.objects.all()  # Fetch all products from the database
+    context = {'products': products}  # Add the products to the context
+    return render(request, 'store/store.html', context)
 
 def cart(request):
-     context = {}
-     return render(request, 'store/cart.html', context)
+    context = {}
+    return render(request, 'store/cart.html', context)
 
 def checkout(request):
-      context = {}
-      return render(request, 'store/checkout.html', context)
+    context = {}
+    return render(request, 'store/checkout.html', context)
